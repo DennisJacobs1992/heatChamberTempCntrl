d91896a28558db234c8ff82cc5d9c3125bceb6e1
Dennis Jacobs on desktop <dennis.jacobs@hotmail.com>
2021-05-25 19:40:55 +0200

    fixed another error

--- ex02_lnx_btn_txt.c
+++ ex02_lnx_btn_txt.c
@@ -149,6 +149,7 @@ int main( int argc, char* args[] )
 {
   bool              bOk = true;
   gslc_tsElemRef*   pElemRef = NULL;
+  char              acTxt[100];
 
   // -----------------------------------
   // Initialize
